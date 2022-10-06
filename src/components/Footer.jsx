b581387eee1ddd6556e3cd81a76b822d92f95961
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-06 00:32:54 -0500

    Add: Component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,60 @@
+import {
+  ButtonGroup,
+  Container,
+  IconButton,
+  Stack,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+const Footer = () => {
+  const { colorMode } = useColorMode();
+  return (
+    <Container
+      as="footer"
+      role="contentinfo"
+      py={2}
+      borderTop="1px"
+      borderColor="rgba(150,150,150,.1)"
+      w="100%"
+      maxW="container.lg"
+    >
+      <Stack spacing={{ base: '2', md: '3' }}>
+        <Stack justify="center" direction="row" align="center">
+          <ButtonGroup variant="ghost">
+            <IconButton
+              as="a"
+              href="#"
+              aria-label="LinkedIn"
+              icon={<FaLinkedin fontSize="1.25rem" />}
+            />
+            <IconButton
+              as="a"
+              href="#"
+              aria-label="GitHub"
+              icon={<FaGithub fontSize="1.25rem" />}
+            />
+            <IconButton
+              as="a"
+              href="#"
+              aria-label="Twitter"
+              icon={<FaTwitter fontSize="1.25rem" />}
+            />
+          </ButtonGroup>
+        </Stack>
+        <Text
+          fontSize="sm"
+          mt={0}
+          color={colorMode === "dark" ? "brand.clear" : "brand.primary"}
+          opacity={'.6'}
+          textAlign={'center'}
+        >
+          &copy; {new Date().getFullYear()} MaxSolFar, Inc. All rights reserved.
+        </Text>
+      </Stack>
+    </Container>
+  );
+};
+
+export default Footer;
