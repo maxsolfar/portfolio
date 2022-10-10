10d123dcd583d8b9d0b6f2a820a8be7c5cfd06c5
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-09 19:34:43 -0500

    Add: Index components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SkillSlider from "./SkillSlider";
+import Header from "./Header";
+
+export { Navbar, Footer, SkillSlider, Header };
