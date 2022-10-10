d3a043c28ad1535f1fdda9bced1c6f85588ad7e8
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-09 23:50:17 -0500

    Add: Configure Router for Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import {
-  VStack,
-} from '@chakra-ui/react';
-import { Footer, Header, Navbar, SkillSlider } from './components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {HomePageEN, HomePageES} from './pages/';
 
 export const App = () => {
-
   return (
-    <VStack minH={"100vh"} fontFamily={"Poppins"}>
-      <Navbar />
-      <Header />
-      <SkillSlider />
-      <Footer />
-    </VStack>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePageEN />} />
+        <Route path="/es" element={<HomePageES />} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 };
