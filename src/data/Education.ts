0113369c831ddfa53aaacc1d02b9f159bc6917b3
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-17 01:14:13 -0500

    Add: Education data

---
+++ src/data/Education.ts
@@ -0,0 +1,31 @@
+const education = [
+  {
+    id: '001',
+    location: 'ISAM - Sabio Antúnez de Mayolo',
+    title: 'Information Systems Development',
+    description: 'Analyze and implement services related to information technologies.',
+    image: 'https://i.imgur.com/6HOKzxF.png',
+    date: '2020 - JAN 2023',
+    certificate: ''
+  },
+  {
+    id: '002',
+    location: 'Bootcamp SOY HENRY',
+    title: 'Full Stack Developer - PERN',
+    description: 'Bootcamp with +800 hours of programming based on real projects.',
+    image: 'https://i.imgur.com/APIuKms.jpg',
+    date: 'APR - SEP 2022',
+    certificate: 'https://certificates.soyhenry.com/cert?id=4166b22c-0502-4e09-aa74-8951458ef0da'
+  },
+  {
+    id: '003',
+    location: 'Platzi',
+    title: 'Full Stack Developer - MERN',
+    description: 'Learning path front end and back end web development with MERN Stack.',
+    image: 'https://i.imgur.com/SsmsdTz.jpg',
+    date: 'JAN - MAY 2022',
+    certificate: 'https://platzi.com/p/maxsolfar/learning-path/100-escuela-javascript/diploma/detalle/'
+  },
+];
+
+export default education;
