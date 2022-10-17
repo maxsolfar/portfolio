9ce5ee7b9993e50413bdec11e1cca3bea40089f8
Maximo Solis Farfan <maxsolfar@gmail.com>
2022-10-17 01:17:20 -0500

    Add: Interface for Education Component

---
+++ src/interfaces/Education.ts
@@ -0,0 +1,17 @@
+export interface EducationListProps {
+  resume: Education[];
+}
+
+export interface Education {
+  id: string;
+  location: string;
+  title: string;
+  description: string;
+  image: string;
+  date: string;
+  certificate: string;
+}
+
+
+
+ 
